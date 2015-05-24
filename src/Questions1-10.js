2227ad26b62eb25a47d57ff3b6c9b4ba065c5e33
barr-code <vb329@georgetown.edu>
2015-05-24 12:49:10 +0100

    Tests 1-3 passing

--- src/Questions1-10.js
+++ src/Questions1-10.js
@@ -1,19 +1,37 @@
 // Question 1
 
 function selectElementsStartingWithE(array) {
-
+  startsWithE = []
+  for(var x = 0; x < array.length; x++){
+    if(array[x][0] === "e"){
+      startsWithE.push(array[x]);
+    }
+  }
+  return startsWithE;
 };
 
 // Question 2
 
 function selectElementsStartingWithVowel(array) {
-
+  startsWithVowel = []
+  for(var x = 0; x < array.length; x++){
+    if(array[x][0].match(/[aeiou]/)){
+      startsWithVowel.push(array[x]);
+    }
+  }
+  return startsWithVowel;
 };
 
 // Question 3
 
 function removeNullsFromArray(array) {
-
+  noNulls = []
+  for(var x = 0; x < array.length; x++){
+    if(array[x] !== null){
+      noNulls.push(array[x]);
+    }
+  }
+  return noNulls;
 };
 
 // Question 4
@@ -55,5 +73,5 @@ function returnFirstHalfOfString(string) {
 // Question 10
 
 function makeNumberNegative(number) {
-  
+
 };
